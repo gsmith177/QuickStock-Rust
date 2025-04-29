6316a2f145e519e8b880019cd74ab5bd2d1763c1
gsmith177 <gsmith17@uccs.edu>
2025-04-29 12:27:06 -0600

    Deleted unused rust files

--- src/tablereader.rs
+++
@@ -1,9 +0,0 @@
-use crate::inventory::InventoryItem;
-use sqlx::SqlitePool;
-
-pub async fn get_inventory(pool: &SqlitePool) -> Result<Vec<InventoryItem>, sqlx::Error> {
-    let items = sqlx::query_as::<_, InventoryItem>("SELECT * FROM inventory")
-        .fetch_all(pool)
-        .await?;
-    Ok(items)
-}
